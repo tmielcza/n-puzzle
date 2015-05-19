6576ef8ef34b0f99c4ba80a1e3b5d282a809b240
Tom MIELCZAREK <tmielcza@e2r1p4.42.fr>
2015-05-19 17:29:57 +0200

    Desnalization function.

--- AStarSolver.cpp
+++ AStarSolver.cpp
@@ -10,6 +10,46 @@ bool AStarSolver::solve(char **map, size_t size) {
   return true;
 }
 
+char **getSnailForm(char **map, int size) {
+  char **newMap = new char*[size * size];
+  int total_size = size * size;
+  int x = 0;
+  int y = 0;
+  int ix = 1;
+  int iy = 0;
+
+  for (int i = 1; i < total_size; i++)
+  {
+    newMap[i] = &map[y][x];
+    if (x + ix < 0 || x + ix >= size)
+    {
+      iy = ix;
+      ix = 0;
+    }
+    else if (y + iy < 0 || y + iy >= size)
+    {
+      ix = -iy;
+      iy = 0;
+    }
+    else if (newMap[y + iy][x + ix] != 0)
+    {
+      if (iy != 0)
+      {
+        ix = -iy;
+        iy = 0;
+      }
+      else
+      {
+        iy = ix;
+        ix = 0;
+      }
+    }
+    x += ix;
+    y += iy;
+  }
+  return (newMap);
+}
+
 char **AStarSolver::finalSolution(int size) {
   char **newMap = new char*[size];
   int total_size = size * size;
@@ -33,7 +73,7 @@ char **AStarSolver::finalSolution(int size) {
       ix = -iy;
       iy = 0;
     }
-    if (newMap[y + iy][x + ix] != 0)
+    else if (newMap[y + iy][x + ix] != 0)
     {
       if (iy != 0)
       {
